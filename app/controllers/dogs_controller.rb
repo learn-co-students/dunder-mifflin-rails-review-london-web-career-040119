3cb56da999db0d888352cfac93cd3ff90b2aed2d
Nahit-1 <nahit86@outlook.com>
2019-05-02 16:54:39 +0100

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,29 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,37 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update  
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to employee_path
+  end 
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create 
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to employees_path
+  end 
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  private 
+
+  def employee_params
+    params.require(:employee).permit!
+  end 
+
 end
